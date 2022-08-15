f6c575a72a5cfbc5019eba5c1b0493a48abb07ac
feat630 <630@hansung.ac.kr>
2022-08-16 07:29:07 +0900

    git test

--- img-in-db/my-app/src/App.js
+++ img-in-db/my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          setting...
+          first setting end
         </p>
         <a
           className="App-link"
